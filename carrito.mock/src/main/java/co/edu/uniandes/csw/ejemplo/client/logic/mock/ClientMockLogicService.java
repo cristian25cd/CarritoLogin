47294bc80a1e3fceef5b6cd31eb897dedf72f0ea
damayor <dr.mayorga20@uniandes.edu.co>
2014-12-01 14:41:28 -0500

    Carrito con Login working. No olvide crear el Realm en glassfish

--- carrito.mock/src/main/java/co/edu/uniandes/csw/ejemplo/client/logic/mock/ClientMockLogicService.java
+++ carrito.mock/src/main/java/co/edu/uniandes/csw/ejemplo/client/logic/mock/ClientMockLogicService.java
@@ -33,8 +33,7 @@ import javax.enterprise.inject.Alternative;
 import javax.inject.Singleton;
 @Singleton
 public class ClientMockLogicService extends _ClientMockLogicService implements IClientLogicService {
-	
-    public ClientDTO getClientId(String name) {
+	public ClientDTO getClientId(String name) {
         return null;
-    }
+        }
 }
